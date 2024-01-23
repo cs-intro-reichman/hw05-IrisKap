6c13fb3ba5f07f303071c991e35c39cfa2ed676a
Iris Kaplun <iris.kaplun@post.runi.ac.il>
2024-01-23 17:32:46 +0200

    fix

--- GameOfLife.java
+++ GameOfLife.java
@@ -197,7 +197,7 @@ public class GameOfLife {
 		{
 			for(int j =1; j<colLength-1; j++ )
 			{
-				System.out.print(arr[i][j] + " ");
+				System.out.print("  "+ arr[i][j]);
 			}
 			System.out.println();
 		}
